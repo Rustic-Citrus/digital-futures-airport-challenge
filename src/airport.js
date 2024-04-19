146c4dfd0bb7f105a2df12cf0b9a4beb17a23ec9
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-19 16:51:32 +0100

    feat: added Airport class to airport.js
    feat: added moveVehicleToAirport method to Airport class
    
    feat: added getTotalVehicles method to Airport class

--- src/airport.js
+++ src/airport.js
@@ -1,5 +1,17 @@
 export default class Airport {
   constructor() {
-    this.id = 1;
+    this.vehicles = new Array();
+  }
+
+  moveVehicleToAirport(aircraft) {
+    this.vehicles.push(aircraft);
+  }
+
+  getTotalVehicles() {
+    let total = 0;
+    this.vehicles.forEach(() => {
+      total += 1;
+    })
+    return total;
   }
 }
