ced97851d860236ee054b2c798c5f5ec8d55f318
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-21 15:34:08 +0100

    feat: added condition to setCapacity method of Airport class

--- src/airport.js
+++ src/airport.js
@@ -31,6 +31,6 @@ export default class Airport {
   }
 
   setCapacity(newCapacity) {
-    if (newCapacity >= 0 && newCapacity > this.capacity) this.capacity = newCapacity;
+    if (newCapacity >= 0 && newCapacity > this.capacity && newCapacity < 50) this.capacity = newCapacity;
   }
 }
