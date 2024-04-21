b353b82c10aa5c094e63e60a3c4d83e6bd345e33
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-21 14:25:15 +0100

    refactor: abstracted out generateRandomAircraft function

--- spec/airport.test.js
+++ spec/airport.test.js
@@ -2,18 +2,29 @@ import Airport from "../src/airport.js";
 import Aircraft from "../src/aircraft.js";
 import airlines from "../data/airlines.json" assert { type: "json" };
 
+function generateRandomAircraft(numOfAircraft) {
+  const aircraftArray = new Array();
+
+  for (let i = 0; i < numOfAircraft; i++) {
+    const seedOne = Math.floor(Math.random() * airlines.length);
+    const seedTwo = Math.floor(Math.random() * 900) + 100 // Random three-digit flight number
+    const aircraft = new Aircraft(airlines[seedOne].name, `${airlines[seedOne].prefix}${seedTwo}`);
+
+    aircraftArray.push(aircraft);
+  }
+
+  return aircraftArray;
+}
+
 export function testTotalAircraft() {
   console.log("TEST: The user can retrieve the total number of aircraft that are in the airport.");
   const airport = new Airport();
   const expectedNumberOfAircraft = 15;
 
-  for (let i = 0; i < 15; i++) {
-    const seedOne = Math.floor(Math.random() * airlines.length);
-    const seedTwo = Math.floor(Math.random() * 900) + 100 // Random three-digit flight number
-    const aircraft = new Aircraft(airlines[seedOne].name, `${airlines[seedOne].prefix}${seedTwo}`);
-
+  const randomAircraft = generateRandomAircraft(15);
+  randomAircraft.forEach((aircraft) => {
     airport.moveVehicleToAirport(aircraft);
-  }
+  });
 
   const actualNumberOfAircraft = airport.getTotalVehicles();
 
