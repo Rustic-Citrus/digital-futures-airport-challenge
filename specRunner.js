7c16721068ffcd45391984cea226ee2150d96099
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-21 16:19:42 +0100

    test: updated specRunner.js

--- specRunner.js
+++ specRunner.js
@@ -1,6 +1,18 @@
 import airportTests from "./spec/airport.test.js";
+import aircraftTests from "./spec/aircraft.test.js";
+
+console.log("=== AIRPORT CLASS TESTS ===");
 
 airportTests.forEach((test, i) => {
   console.log(`=== TEST #${i + 1} ===`);
   test();
 });
+
+console.log("=== AIRCRAFT CLASS TESTS ===")
+
+aircraftTests.forEach((test, i) => {
+  console.log(`=== TEST #${i + 1} ===`);
+  test();
+});
+
+console.log("=== ALL TESTS COMPLETE ===");
